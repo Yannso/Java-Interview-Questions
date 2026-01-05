f6cc73e6fab66f127b3648cd6b338031fe1715f0
yannso <9392653+yannso@user.noreply.gitee.com>
2026-01-05 18:26:36 +0800

    更新了1411 给 N x 3 网格图涂色方案数

--- src/main/java/com/yanso/dp/NumOfWays.java
+++ src/main/java/com/yanso/dp/NumOfWays.java
@@ -6,7 +6,7 @@ public class NumOfWays {
         int[][] dp = new int[n][2];
         dp[0][0] = dp[0][1] = 6;
         for (int i = 1; i < n; i++) {
-            // 使用 long 类型承接中间计算结果
+            // 一定要使用long变量作为中间结果
             long aba = dp[i - 1][0]; // 可以提供3个ABA方案，2个ABC方案
             long abc = dp[i - 1][1]; // 可以提供2个ABA方案，2个ABC方案
 
