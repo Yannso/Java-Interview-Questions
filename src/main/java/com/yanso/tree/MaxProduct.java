b808f11f4819b41a2edec9822acd62ee076e3011
yannso <9392653+yannso@user.noreply.gitee.com>
2026-01-07 01:20:32 +0800

    更新 1339. 分裂二叉树的最大乘积

---
+++ src/main/java/com/yanso/tree/MaxProduct.java
@@ -0,0 +1,42 @@
+package com.yanso.tree;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MaxProduct {
+    public static int MOD = 1000000007;
+    public long result = Integer.MIN_VALUE;
+
+    public int maxProduct(TreeNode root) {
+        long totalSum = dfsGetSum(root);
+        dfs(root, totalSum);
+        return (int) (result % MOD);
+    }
+
+    private long dfsGetSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return dfsGetSum(root.left) + dfsGetSum(root.right) + root.val;
+    }
+
+    private int dfs(TreeNode root, long totalSum) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left, totalSum);
+        int right = dfs(root.right, totalSum);
+        long curSum = left + right + root.val;
+        result = Math.max(result, curSum * (totalSum - curSum));
+        return left + right + root.val;
+    }
+
+    public static void main(String[] args) {
+        Integer[] rootArray = {1,null,2,3,4,null,null,5,6};
+        TreeNode root = TreeUtils.fromArray(rootArray);
+        TreeUtils.printTree(root);
+        int result = new MaxProduct().maxProduct(root);
+        System.out.println("result = " + result);
+    }
+
+}
--- src/main/java/com/yanso/tree/TreeUtils.java
+++ src/main/java/com/yanso/tree/TreeUtils.java
@@ -1,42 +1,59 @@
 package com.yanso.tree;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 public class TreeUtils {
     public static TreeNode fromArray(Integer[] array) {
-        if (array == null || array.length == 0) {
-            return null;
-        }
-        return dfsCreateTree(array,0);
-    }
+        if (array == null || array.length == 0) return null;
 
-    private static TreeNode dfsCreateTree(Integer[] array, int i) {
-        if (i >= array.length || array[i] == null) {
-            return null;
+        TreeNode root = new TreeNode(array[0]);
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        int i = 1;
+        while (i < array.length) {
+            TreeNode curr = queue.poll();
+
+            // 处理左子节点
+            if (i < array.length && array[i] != null) {
+                curr.left = new TreeNode(array[i]);
+                queue.add(curr.left);
+            }
+            i++;
+
+            // 处理右子节点
+            if (i < array.length && array[i] != null) {
+                curr.right = new TreeNode(array[i]);
+                queue.add(curr.right);
+            }
+            i++;
         }
-        TreeNode rootNode = new TreeNode(array[i]);
-        rootNode.left = dfsCreateTree(array, 2 * i + 1);
-        rootNode.right = dfsCreateTree(array, 2 * i + 2);
-        return rootNode;
+        return root;
     }
 
     public static void printTree(TreeNode root) {
         if (root == null) {
+            System.out.println("[]");
             return;
         }
-        Deque<TreeNode> deque = new ArrayDeque<>();
-        deque.offer(root);
-        while (!deque.isEmpty()) {
-            TreeNode current = deque.poll();
-            System.out.print(current.val + " ");
-            if (current.left != null) {
-                deque.offer(current.left);
-            }
-            if (current.right != null) {
-                deque.offer(current.right);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        List<String> res = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            TreeNode curr = queue.poll();
+            if (curr == null) {
+                res.add("null");
+            } else {
+                res.add(String.valueOf(curr.val));
+                queue.offer(curr.left);
+                queue.offer(curr.right);
             }
         }
-        System.out.println();
+        // 移除末尾多余的 null
+        while (res.get(res.size() - 1).equals("null")) {
+            res.remove(res.size() - 1);
+        }
+        System.out.println(res);
     }
 }
