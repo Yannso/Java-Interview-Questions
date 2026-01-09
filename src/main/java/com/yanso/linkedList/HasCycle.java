6c86091aaa2f42778dcee072ab152ca128bb2189
yannso <9392653+yannso@user.noreply.gitee.com>
2026-01-09 14:12:47 +0800

    更新141. 环形链表

---
+++ src/main/java/com/yanso/linkedList/HasCycle.java
@@ -0,0 +1,14 @@
+package com.yanso.linkedList;
+
+public class HasCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
