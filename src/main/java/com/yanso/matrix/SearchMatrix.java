588ac8184ee5d45d370ade4ddd00f83cf5f5c542
yannso <9392653+yannso@user.noreply.gitee.com>
2026-01-09 13:40:37 +0800

    更新240. 搜索二维矩阵2

---
+++ src/main/java/com/yanso/matrix/SearchMatrix.java
@@ -0,0 +1,29 @@
+package com.yanso.matrix;
+
+public class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int x = 0, y = n - 1;
+        while (x < m && y >= 0) {
+            if (matrix[x][y] == target) return true;
+            else if (matrix[x][y] > target) y--;
+            else x++;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,4,7,11,15},
+                {2,5,8,12,19},
+                {3,6,9,16,22},
+                {10,13,14,17,24},
+                {18,21,23,26,30}
+        };
+        int target = 5;
+        boolean isSuccess = new SearchMatrix().searchMatrix(matrix, target);
+        System.out.println("isSuccess = " + isSuccess);
+    }
+}
